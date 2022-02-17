cb4bb19dd6ba1bac4a46da4cfaef4f02ae6baefb
Joel Munoz <j.munoz1@yahoo.com>
2022-02-17 09:44:53 -0800

    console error fixed

--- src/Row.js
+++ src/Row.js
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
 
-function Row({title}) {
+function Row({ title, fetchUrl }) {
 
     const [movies, setMovies] = useState([]);
 
     //snippet of code for specific condition
-    useEffect(()=>{
+    useEffect(() => {
         // if [], run once when the row loads and dont run again
+        // asyncronus call
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
 
-    },[]);
 
-    
     return (
         <div>
-        {/* titls */}
-        <h2>{title}</h2>
-        {/* constainer -> images */}
+            {/* titls */}
+            <h2>{title}</h2>
+            {/* constainer -> images */}
 
         </div>
     )
--- src/request.js
+++ src/request.js
@@ -1,9 +1,9 @@
 
-const API_KEY = process.env.TMDB_API_KEY;
+const API_KEY = "92684c3f5ec92c92f29964092b3a4992";
 
 const request = {
     fetchingTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchingNetflixOriginals: `/discover/tv?api_key=${API_KEY}$with_networks=2134`,
+    fetchingNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchedActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
