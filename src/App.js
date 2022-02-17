d4074880505254dc174629495e4290dcaf461ef3
Joel Munoz <j.munoz1@yahoo.com>
2022-02-17 09:22:35 -0800

    files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Row from './Row';
+import request from './request';
 
 function App() {
   return (
     <div className="App">
-    <h1> Hi</h1>
-      
+      <h1>Hi</h1>
+      <Row title="Netflix originals" fetchUrl={request.fetchingNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={request.fetchingTrending} />
     </div>
   );
 }
