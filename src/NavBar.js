6a0ff6bac0c117af3ecf5439597c1dca7855fcfd
Joel Munoz <j.munoz1@yahoo.com>
2022-02-17 17:22:24 -0800

    nav bar finished

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./style/NavBar.css";
 
 function NavBar() {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setShow(true);
+            } else setShow(false);
+        });
+
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, []);
+
     return (
-        <div className='nav'>
+        <div className={`nav ${show && "nav-black"}`}>
             <img
                 className='nav-logo'
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
